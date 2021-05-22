032c1d814ca8062cd6fd1273e27ac448fba2db49
HlaingMinThan <hlaingminthan92@gmail.com>
2021-05-22 20:52:56 +0630

    python-lesson-5-input-box-and-type-casting

---
+++ main.py
@@ -0,0 +1,14 @@
+# name=input(' Name :');
+# print('hello '+name)
+
+# age=input('tell me your age:');
+# print('your age is '+ age)
+
+# area calculator (pi r**2)
+
+PI=3.14;
+r=int(input('radius: '));
+
+area=PI*r**2;
+
+print(area);
