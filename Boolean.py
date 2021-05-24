f2b85de3d0287743ce99cc07bba37c4cd087d859
HlaingMinThan <hlaingminthan92@gmail.com>
2021-05-24 11:19:28 +0630

    python-lesson-7-boolean-and-operators_1

---
+++ Boolean.py
@@ -0,0 +1,21 @@
+# hungry=False
+
+# print(hungry);
+
+# comparison operator > < >= <= ==
+
+# print(5<3)
+
+# print(5==3)
+
+# print(5>=5)
+
+# print(3<=3);
+
+# logical operator (and or not)
+
+# print(not False);
+
+# print(5>13 and  5>10)
+
+print(5>13 or 5>12)
