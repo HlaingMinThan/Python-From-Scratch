aca9ac51d700890127931cfb0cad7abc9593a6f7
HlaingMinThan <hlaingminthan92@gmail.com>
2021-05-25 17:42:33 +0630

    python-lesson-8-if-else-conditions

---
+++ if_else.py
@@ -0,0 +1,21 @@
+# age = int(input('age :'))
+
+# if age < 18 :
+#     print('you are young')
+# elif age > 18 and age < 30 :
+#     print('you are normal age');
+# else :
+#     print('you are old');
+
+    # print('hi'); 
+
+tired=input('are u tired? "y/n"') # y or n
+
+if tired == 'y' :
+    print('rest well');
+elif tired =='n' :
+    print('go back to work');
+else : 
+    print('pls enter y or n');
+
+
