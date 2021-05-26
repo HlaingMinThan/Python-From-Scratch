1b39df891b01b4aec9beed4cb42db7a5c194c8c8
HlaingMinThan <hlaingminthan92@gmail.com>
2021-05-26 16:50:12 +0630

    python-lesson-9-for-loop

---
+++ loops.py
@@ -0,0 +1,13 @@
+# names=['Aung aung','Kyaw kyaw','Hlaing Min Than','Susu'];
+
+# for name in names :
+#     if name == 'Hlaing Min Than' :
+#         print(f'{name} is instructor')
+#         break
+#     else :
+#         print(f'{name} is student')
+
+fruits=['apple','orange','watermelon'];
+
+for fruit in fruits :
+    print(f'{fruit} is a fruit');
