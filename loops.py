ab4958cb1eb6fd4adcf5a131d8495e5227b8e2cc
HlaingMinThan <hlaingminthan92@gmail.com>
2021-05-26 22:45:41 +0630

    python-lesson-10-while-loop-101

--- loops.py
+++ loops.py
@@ -1,13 +1,8 @@
-# names=['Aung aung','Kyaw kyaw','Hlaing Min Than','Susu'];
-
-# for name in names :
-#     if name == 'Hlaing Min Than' :
-#         print(f'{name} is instructor')
-#         break
-#     else :
-#         print(f'{name} is student')
-
-fruits=['apple','orange','watermelon'];
-
-for fruit in fruits :
-    print(f'{fruit} is a fruit');
+# while loop
+num=0
+while num<10 :
+    if num > 5 :
+        break
+    if num % 2 == 0 :
+        print(f'{num}')
+    num+=1;
