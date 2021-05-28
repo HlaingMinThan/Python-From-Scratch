ebc28e30a5dd8f5cd25866840bcc55fd45f16cbe
HlaingMinThan <hlaingminthan92@gmail.com>
2021-05-28 20:04:37 +0630

    python-lesson-11-ranges

---
+++ range.py
@@ -0,0 +1,10 @@
+# nums=[3,4];
+
+# for num in range(20) :
+#     print('hello world');
+
+pizzas=['chessy','spicy','mayo chicken','chicken chessy','spicy chicken']
+
+# print(pizzas[0]);
+for num in range(len(pizzas)) :
+    print(pizzas[num])
