faa23bde16cd1f9b07ce8ea24feca2a3ca473a63
Hlaing Min Than(Faizal) <hlaingminthan92@gmail.com>
2021-07-04 01:41:17 +0630

    what-are-dictonaries-in-python

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+person={
+    'name':'hlaing min than',
+    'age':20
+}
+# person['hair_color']="black";
+# print(person);
+
+# if 'nothing' in person :
+#     print('work');
+
+# person_keys=list(person.keys())
+# print(person_keys)
+
+# person_values=list(person.values());
+# print(person_values);
