f403ad7ea1231d0a6f02780cfc469c8c64bbca39
Hlaing Min Than(Faizal) <hlaingminthan92@gmail.com>
2021-07-06 01:49:17 +0630

    python-lesson-16-sorted-function-set-function-concept

--- example1.py
+++ example1.py
@@ -11,5 +11,11 @@ while True :
     else :
         break
 
-for (key,value) in person.items():
-    print(f'{key} is {value} years old ')
+ages=list(person.values())#['20', '40', '20']
+
+for age in set(ages) :
+    count=ages.count(age)
+    print(f'{age} years old - {count}'); 
+    
+    # 20 years old - 2
+    # 40 years old - 1
---
+++ sorted_set.py
@@ -0,0 +1,8 @@
+ages=[20,40,50,20]
+
+for age in sorted(set(ages)) :
+    print(age);
+
+# names=['hlaing min than','Aung Aung','Kyaw kyaw','bobo','aung la'];
+
+# print(sorted(names))
