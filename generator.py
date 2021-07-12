69db65872bc07f7ca16a527878ad9f9b5639e38a
Hlaing Min Than(Faizal) <hlaingminthan92@gmail.com>
2021-07-12 22:33:50 +0630

    python-lesson-29-mixing-custom-word-in-gibberish-generator

--- generator.py
+++ generator.py
@@ -1,8 +1,16 @@
+
+words=['apple','orange','lemon','lime','banana'];
+from random import randint;
+def randomSentenceGenerator(word):
+    randomIndex=randint(0,len(words)-1)
+    return f'{words[randomIndex]} {word}'
+
 with open('./text.txt') as file:
     paragraph=file.read();
-
+    wordLists=paragraph.split()#['asdf','asdf]
+    sentenceList=list(map(randomSentenceGenerator,wordLists))#[]
     paraCount=int(input('paragraph count : '))# 3
 
     for count in range(paraCount):
         with open('./generator.txt','a') as write_file:
-            write_file.write(paragraph+'\n\n')
+            write_file.write( ''.join(sentenceList) +'\n\n')
